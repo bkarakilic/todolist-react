88533af322ba8fc17d3d69e31844bf809e09362e
bkarakilic <bilo-aks69@hotmail.com>
2022-10-21 16:01:34 +0200

    update

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Home';
-import Page2 from './Page2';
-
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      // <NavigationContainer>
-      //   <Stack.Navigator>
-      //     <Stack.Screen name="Home" component={Home} />
-      //     <Stack.Screen name="Page2" component={Page2} />
-      //   </Stack.Navigator>
-      // </NavigationContainer>
-
       <Home />
     
   );
